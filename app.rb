f0c6c1e5754fdc1cb2672de5195f0fd887c0c766
vincelep <vincent.leparoux@gmail.com>
2019-01-24 12:13:47 +0100

    mise en forme

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
-require 'bundler' # permet de remplacer tous les require 'gem'
+ ####################################################################################
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   REQUIERED   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
+
+require 'bundler'
 Bundler.require
 
-$:.unshift File.expand_path("./../", __FILE__) # permet de se placer directement dans le lib/app, sans avoir à rentrer le chemin précis
-require 'game'
+####################################################################################
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   METHODS   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
 
-Application.new.perform
+
+$:.unshift File.expand_path("./../lib", __FILE__)
+require 'application'
+
+
+#####################################################################################
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   INITIALIZE   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
+
+                               Application.new.perform
+
+#####################################################################################
